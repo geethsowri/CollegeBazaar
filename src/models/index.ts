71ae5438953b25e9df9bb0e9cc79c689e7391a63
Sowri <geethsowri.n@inncircles.com>
2025-11-17 11:00:00 +0530

    Add barrel export for all Mongoose models

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+export { User } from "./User";
+export { Listing } from "./Listing";
+export { Order } from "./Order";
+export { Otp } from "./Otp";
+export { Wishlist } from "./Wishlist";
+export { Conversation } from "./Conversation";
+export { Message } from "./Message";
+export { Report } from "./Report";
+export { Notification } from "./Notification";
+export { Review } from "./Review";
+export { SavedSearch } from "./SavedSearch";
